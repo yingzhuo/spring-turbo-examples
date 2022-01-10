d5a9d237ba60364957a86267723e0926bdc9dbf4
应卓 <yingzhor@gmail.com>
2022-01-10 18:38:02 +0800

    2022-01-10 18:38:01

--- example-excel-writing/src/main/java/spring/turbo/examples/ApplicationBootMvc.java
+++ example-excel-writing/src/main/java/spring/turbo/examples/ApplicationBootMvc.java
@@ -1,33 +1,9 @@
 package spring.turbo.examples;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.servlet.view.BeanNameViewResolver;
-import spring.turbo.examples.valueobject.Cat;
-import spring.turbo.module.excel.writer.AnnotationDrivenExcelView;
-import spring.turbo.module.excel.writer.WorkbookMetadata;
 
 @Configuration
+@ServletComponentScan
 class ApplicationBootMvc {
-
-    @Bean(name = "cats-view")
-    AnnotationDrivenExcelView catView(ApplicationContext applicationContext) {
-        return new AnnotationDrivenExcelView(
-                applicationContext,
-                WorkbookMetadata.builder()
-                        .workbook("小猫咪.xlsx")
-                        .sheet(Cat.class, 1, "咪咪", "cats")
-                        .sheet(Cat.class, 0, "小猫咪", "cats")
-                        .build()
-        );
-    }
-
-    @Autowired
-    void config(BeanNameViewResolver viewResolver) {
-        viewResolver.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    }
-
 }
--- example-excel-writing/src/main/java/spring/turbo/examples/controller/ExcelExportingController.java
+++ example-excel-writing/src/main/java/spring/turbo/examples/controller/ExcelExportingController.java
@@ -1,28 +1,56 @@
 package spring.turbo.examples.controller;
 
+import lombok.AllArgsConstructor;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 import spring.turbo.examples.valueobject.Cat;
+import spring.turbo.io.PathUtils;
+import spring.turbo.module.excel.writer.WorkbookBuilder;
+import spring.turbo.module.excel.writer.WorkbookIO;
+import spring.turbo.module.security.webmvc.entity.AttachmentResponseEntity;
 
 import java.math.BigDecimal;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 @Controller
-public class ExcelExportingController {
+@AllArgsConstructor
+class ExcelExportingController {
 
-    private final List<Cat> cats = Arrays.asList(
-            new Cat(1L, null, 8, new Date(), null),
+    private final ApplicationContext applicationContext;
+
+    private static final List<Cat> CATS = Arrays.asList(
+            new Cat(1L, "米来", 8, new Date(), BigDecimal.TEN),
             new Cat(2L, "贵霜", 6, null, BigDecimal.ZERO)
     );
 
     @GetMapping("/cats")
-    public ModelAndView cat() {
-        ModelAndView mv = new ModelAndView("cats-view");
-        mv.addObject("cats", cats);
-        return mv;
+    public ResponseEntity<byte[]> cat() {
+        final Workbook workbook = WorkbookBuilder.newInstance()
+                .applicationContext(applicationContext)
+                .sheet(Cat.class, 0, "猫儿", CATS)
+                .build();
+
+        // 生成Excel临时文件
+        final Path tempFile = WorkbookIO.writeToTempFile(workbook, null, null);
+
+        // 下载文件
+        try {
+            return AttachmentResponseEntity.builder()
+                    .attachmentName("小猫咪.xlsx")
+                    .mediaType(MediaType.APPLICATION_OCTET_STREAM)
+                    .content(tempFile)
+                    .build();
+        } finally {
+            // 从服务器上删除临时文件
+            PathUtils.deleteQuietly(tempFile);
+        }
     }
 
 }
