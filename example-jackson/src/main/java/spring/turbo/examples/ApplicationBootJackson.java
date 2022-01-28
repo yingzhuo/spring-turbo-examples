51e209c32ed752780124dff14de7657432f9ec34
应卓 <yingzhor@gmail.com>
2022-01-28 13:47:49 +0800

    2022-01-28 13:47:48

--- example-jackson/src/main/java/spring/turbo/examples/ApplicationBootJackson.java
+++ example-jackson/src/main/java/spring/turbo/examples/ApplicationBootJackson.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import spring.turbo.examples.domain.Cat;
 import spring.turbo.examples.mixin.CatMixIn;
-import spring.turbo.examples.mixin.JsonMixIn;
+import spring.turbo.module.jackson.mixin.predefined.JsonMixIns;
 import spring.turbo.webmvc.api.Json;
 
 import java.util.Optional;
@@ -16,7 +16,7 @@ class ApplicationBootJackson {
     @Autowired
     void configObjectMapper(Optional<ObjectMapper> mapperOption) {
         mapperOption.ifPresent(om -> {
-            om.addMixIn(Json.class, JsonMixIn.class);
+            om.addMixIn(Json.class, JsonMixIns.Style1.class);
             om.addMixIn(Cat.class, CatMixIn.class);
         });
     }
