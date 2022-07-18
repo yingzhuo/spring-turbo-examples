c008f69e2f9c14ad8c6f28b7b549fcf6193c3a56
应卓 <yingzhor@gmail.com>
2022-07-18 16:13:03 +0800

    2022-07-18 16:13:02

--- example-core/src/main/java/spring/turbo/examples/ApplicationBoot.java
+++ example-core/src/main/java/spring/turbo/examples/ApplicationBoot.java
@@ -2,8 +2,6 @@ package spring.turbo.examples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import spring.turbo.module.dirwatcher.EnableDirectoryWatchers;
 
 @SpringBootApplication
 public class ApplicationBoot {
